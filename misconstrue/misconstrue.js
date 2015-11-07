4498b588d5751d89395e2cf33061ed31333c2438
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-11-07 18:48:21 +0000

    slightly better working home screen
    -example code for if need-be
    -added iron:router package

--- misconstrue/misconstrue.js
+++ misconstrue/misconstrue.js
@@ -1,4 +1,5 @@
 if (Meteor.isClient) {
+  Session.setDefault('name', '');
   Template.login.events({
     'submit form': function(e) {
       e.preventDefault();
@@ -7,7 +8,11 @@ if (Meteor.isClient) {
         console.log("No name entered.");
       } else {
         console.log(name);
-        //Call Rowan's thing here.
+        //Example of adding p's:
+        // $('#disp_name').append(
+        //   $('<p></p>').text(name)
+        // );
+        //Call Rowan's thing here. 
       }
     }
   });
