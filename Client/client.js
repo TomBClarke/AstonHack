6f106d7ff7b9fb28e0d04d64736d03f07543e6d3
Misha Wagner <mishajw@gmail.com>
2015-11-07 20:19:22 +0000

    Server logging

--- Client/client.js
+++ Client/client.js
@@ -1,20 +1,35 @@
 var name;
-var socket = new WebSocket("ws://localhost:3000"); // ("ws://82.46.212.232:32596");
-function connect() {
+var socket;
+
+function initialiseSocket() {
+    console.log("Setting up socket");
     
-//    socket.onopen = function() {
-//        console.log("Sending Message");
-//        socket.send("ping");
-//    }
-		
+    socket = new WebSocket("ws://localhost:3000");
+    
     socket.onmessage = function(s) {
 		console.log("Recieved");
 		console.log(s);
+        
+        var message = $.parseJSON(s.data);
 
 		$("#messages").append(
-            $("<div></div>").text(s.data)
+            $("<div></div>")
+                .attr("class", "message-container")
+                .append($("<div></div>")
+                    .attr("class", "message-sender")
+                    .text(message.sender))
+                .append($("<div></div>")
+                    .attr("class", "message-sender")
+                    .text(message.text))
 		);
     }
+}
+
+function registerName() {
+    socket.send(JSON.stringify({
+        type: "nameset",
+        newName: $("#name").val()
+    }));
 }
 
 function send() {
