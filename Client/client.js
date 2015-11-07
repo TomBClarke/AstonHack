5181c34b7b6e815b1e345de7381d18fe68d13e00
Misha Wagner <mishajw@gmail.com>
2015-11-07 18:18:43 +0000

    Server working! + client adapted a bit

--- Client/client.js
+++ Client/client.js
@@ -2,18 +2,25 @@ var name;
 var socket;
 
 function connect() {
-    socket = new WebSocket("ws://82.46.212.232:32596");
-    socket.onopen = function() {
-        console.log("Sending Message");
-        socket.send("ping");
-    }
+    socket = new WebSocket("ws://localhost:3000"); // ("ws://82.46.212.232:32596");
+    
+//    socket.onopen = function() {
+//        console.log("Sending Message");
+//        socket.send("ping");
+//    }
+		
     socket.onmessage = function(s) {
-        console.log("received: " + s);
+		console.log("Recieved");
+		console.log(s);
+
+		$("#messages").append(
+            $("<div></div>").text(s.data)
+		);
     }
 }
 
 function send() {
-    socket.send();
+    socket.send($("#input").val());
 }
 
 function close() {
