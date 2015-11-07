8706a4a7bb1c3669a8b82c15de0d5d44f6f6482c
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-11-07 18:14:49 +0000

    got the first input working

--- misconstrue/misconstrue.js
+++ misconstrue/misconstrue.js
@@ -1,23 +1,14 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.login.events({
+    'submit form': function(e) {
+      e.preventDefault();
+      var name = event.target.name.value;
+      if (name == "") {
+        console.log("No name entered.");
+      } else {
+        console.log(name);
+        //Call Rowan's thing here.
+      }
     }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
