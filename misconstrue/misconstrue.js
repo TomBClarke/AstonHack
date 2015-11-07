ee0e7c2f10272e012505f0292cc3053d4c970eef
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-11-07 20:22:07 +0000

    done some work, preparing other templates

--- misconstrue/misconstrue.js
+++ misconstrue/misconstrue.js
@@ -1,19 +1,31 @@
 if (Meteor.isClient) {
-  Session.setDefault('name', '');
+  //Holds which page is being displayed:
+  Session.setDefault('page', 'login');
+
+  //Used to determine which page should be shown. (WIP)
+  // Template.body.page_is = function(data) {
+  //   if (Session.equals('page', data)) {
+
+  //   }
+  // }
+
+  //Handles the login template:
   Template.login.events({
+    //Reacts to the button pres.
     'submit form': function(e) {
+      //Stops form reloading the page.
       e.preventDefault();
+      //Gets the name the user enterd.
       var name = event.target.name.value;
       if (name == "") {
         console.log("No name entered.");
       } else {
+        //Got a name, let's try to connect.
         console.log(name);
-        //Example of adding p's:
-        // $('#disp_name').append(
-        //   $('<p></p>').text(name)
-        // );
-        //Call Rowan's thing here. 
+        //Call Rowan's thing here. Could do with error handling.
       }
     }
   });
+
+  
 }
